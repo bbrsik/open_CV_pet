fe25eabb35938c8e5f35278c9604f731217ed9ad
unknown <sainte66@gmail.com>
2024-08-04 16:28:00 +0300

    display all images in assets folder done

---
+++ settings.py
@@ -0,0 +1,4 @@
+import os
+
+os.makedirs(name="assets", exist_ok=True)
+ASSETS_DIR = "./assets/"
---
+++ utility.py
@@ -0,0 +1,35 @@
+import os
+
+
+def is_folder_empty(folder: str):
+    if len(os.listdir(folder)) == 0:
+        return True
+    else:
+        return False
+
+
+def should_rename_files_in_folder(folder: str):
+    folder = folder
+    filename_list = os.listdir(folder)
+    if len(filename_list) == 0:
+        return False
+
+    for number, filename in enumerate(filename_list):
+        if str(number) == filename[:len(str(number))]:
+            continue
+        else:
+            return True
+    return False
+
+
+def rename_files_in_folder(folder: str):
+    folder = folder
+    filename_list = os.listdir(folder)
+
+    for number, filename in enumerate(filename_list):
+        source = f"{folder}/{filename}"
+        extension = filename.split(".").pop()
+        destination = f"{str(number)}.{extension}"
+        os.rename(source, f"{folder}/{destination}")
+
+    return 0
